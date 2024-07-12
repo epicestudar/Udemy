5dc572ae54107234d8a6776982ac89ae914ee809
epicestudar <viniciusftza@gmail.com>
2024-07-12 14:05:54 -0300

    comecando novo projeto

---
+++ PHP/Projetos/Agenda/index.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once("templates/header.php");
+
+?>
+<h1>Testando agenda
+</h1>
+
+<?php
+  include_once("templates/footer.php");
+?>
---
+++ PHP/Projetos/Agenda/templates/header.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once('config/url.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+        <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+      </a>
+      <div>
+        <div class="navbar-nav">
+          <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Agenda</a>
+          <a class="nav-link active" href="<?= $BASE_URL ?>create.php">Adicionar Contato</a>
+        </div>
+      </div>
+    </nav>
+  </header>
