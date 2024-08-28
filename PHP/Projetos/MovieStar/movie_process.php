519b4e85ad256252c42f1bab8cbee0fc43c65bee
epicestudar <viniciusftza@gmail.com>
2024-08-28 10:49:34 -0300

    atualizando projeto

--- PHP/Projetos/MovieStar/movie_process.php
+++ PHP/Projetos/MovieStar/movie_process.php
@@ -76,6 +76,69 @@ if ($type === "create") {
   }
 }
 
+else if($type === "update") {
+  $title = filter_input(INPUT_POST, "title");
+    $description = filter_input(INPUT_POST, "description");
+    $trailer = filter_input(INPUT_POST, "trailer");
+    $category = filter_input(INPUT_POST, "category");
+    $length = filter_input(INPUT_POST, "length");
+    $id = filter_input(INPUT_POST, "id");
+
+    $movieData = $movieDAO->findById($id);
+
+    if($movieData) {
+      if($movieData->users_id === $userData->id) {
+        if (!empty($title) && !empty($description) && !empty($category)) {
+          $movieData->title = $title;
+        $movieData->description = $description;
+        $movieData->trailer = $trailer;
+        $movieData->category = $category;
+        $movieData->length = $length;
+
+        if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+
+          $image = $_FILES["image"];
+          $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+          $jpgArray = ["image/jpeg", "image/jpg"];
+  
+          // Checando tipo da imagem
+          if(in_array($image["type"], $imageTypes)) {
+  
+            // Checa se imagem é jpg
+            if(in_array($image["type"], $jpgArray)) {
+              $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+            } else {
+              $imageFile = imagecreatefrompng($image["tmp_name"]);
+            }
+
+            $movie = new Movie();
+  
+            // Gerando o nome da imagem
+            $imageName = $movie->imageGenerateName();
+  
+            imagejpeg($imageFile, "./img/movies/" . $imageName, 100);
+  
+            $movieData->image = $imageName;
+  
+          } else {
+  
+            $message->setMessage("Tipo inválido de imagem, insira png ou jpg!", "error", "back");
+  
+          }
+      }
+      $movieDAO->update($movieData);
+        } else{
+          $message->setMessage("Você precisa adicionar o titulo, a descrição e a categoria do filme!", "error", "back");
+        }
+        
+      } else{
+        $message->setMessage("Informações inválidas!", "error", "index.php");
+      }
+    } else{
+      $message->setMessage("Informações inválidas!", "error", "index.php");
+    }
+}
+
 else {
     $message->setMessage("Informações inválidas!", "error", "index.php");
 }
