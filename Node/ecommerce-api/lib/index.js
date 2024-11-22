1d15093f32493fd9b4521a4306ce8ce25b844bf6
epicestudar <viniciusftza@gmail.com>
2024-11-22 10:08:44 -0300

    configuração do tsc-watch no projeto

--- Node/ecommerce-api/lib/index.js
+++ Node/ecommerce-api/lib/index.js
@@ -3,6 +3,16 @@ const app = express();
 app.get("/", (_req, res) => {
     res.send("Bem vindo ao curso de Node.js");
 });
+app.get("/users", (_req, res) => {
+    let usuarios = [{
+            "nome": "Vinícius",
+            "idade": 18
+        }, {
+            "nome": "Derci",
+            "idade": 33
+        }];
+    res.send(usuarios);
+});
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000");
 });
--- Node/ecommerce-api/src/index.ts
+++ Node/ecommerce-api/src/index.ts
@@ -6,6 +6,18 @@ app.get("/", (_req: Request, res: Response) => {
   res.send("Bem vindo ao curso de Node.js");
 });
 
+app.get("/users", (_req: Request, res: Response) => {
+    let usuarios = [{
+        "nome": "Vinícius",
+        "idade": 18
+    }, {
+        "nome": "Derci",
+        "idade": 33
+    }];
+
+    res.send(usuarios);
+});
+
 app.listen(3000, () => {
   console.log("Servidor ativo na porta 3000");
 });
