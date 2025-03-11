e5b592c386f71da895d8eee8583864fbecf19150
epicestudar <viniciusftza@gmail.com>
2025-03-11 10:43:45 -0300

    primeiro projeto angular

---
+++ Angular/Secao2/projeto-inicial/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent, Comp2Component]
+})
+export class AppModule { }
---
+++ Angular/Secao2/projeto-inicial/src/app/comp2/comp2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp2',
+  standalone: false,
+  templateUrl: './comp2.component.html',
+  styleUrl: './comp2.component.scss'
+})
+export class Comp2Component {
+
+}
