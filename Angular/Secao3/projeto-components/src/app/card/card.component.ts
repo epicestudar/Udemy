f8452dcda052bf0e42df25e7b8f7a116bfa77952
epicestudar <viniciusftza@gmail.com>
2025-03-14 11:20:41 -0300

    interpolação

--- Angular/Secao3/projeto-components/src/app/card/card.component.ts
+++ Angular/Secao3/projeto-components/src/app/card/card.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  plano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 100,
+    },
+  };
 }
