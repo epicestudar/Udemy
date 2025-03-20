258c0bd33d18d9d21149e94837e8a7d29cc9f29e
epicestudar <viniciusftza@gmail.com>
2025-03-20 11:35:51 -0300

    inicio da secao 4 - conceitos de property binding

---
+++ Angular/Secao4/property-binding/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  inputText = "Texto Inicial";
+  inputType = "text";
+  isDisabled = false;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  setPasswordTypeInput() {
+    this.inputType = "password";
+  }
+
+  setTextTypeInput() {
+    this.inputType = "text";
+  }
+}
